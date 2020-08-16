1387a7454e10e5de25b2e0443f2989ef89dfad89
TasosVellis <anastasios.vellis@gmail.com>
2020-08-16 16:31:43 +0300

    Locators Tutorial_CSS_Xpath

--- PythonSelenium/Locators.py
+++ PythonSelenium/Locators.py
@@ -8,3 +8,8 @@ driver.find_element_by_css_selector("input[name='name']").send_keys("Tasos")
 driver.find_element_by_name("email").send_keys("mail@mail.com")
 driver.find_element_by_xpath("//input[@type='submit']").click()
 driver.find_element_by_id("exampleCheck1").click()
+
+print(driver.find_element_by_class_name("alert-success").text)
+print(driver.find_element_by_css_selector("[class*='alert-success']").text)
+# //*[contains(@class,'alert-success')] -XPath Regex
+# [class*='alert-success' -CSS Regex
