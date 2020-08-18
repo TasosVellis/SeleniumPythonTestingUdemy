cd52ebe742ac5206a5e90af764218fca826fa462
TasosVellis <anastasios.vellis@gmail.com>
2020-08-19 01:41:16 +0300

    Finalize Functional Automation and Example 2

--- PythonSelenium/Assignment.py
+++ PythonSelenium/Assignment.py
@@ -11,73 +11,33 @@
 # 4 Verify if Search functionality in home page is working, put ca and see 4 products are x,y z
 
 import time
-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.wait import WebDriverWait
+
 
 driver = webdriver.Chrome(executable_path="C:\\Drivers\\chromedriver.exe")
-veg_list = []
-items_list = []
 driver.get("https://rahulshettyacademy.com/seleniumPractise/")
+
+validation_list = ["Cauliflower - 1 Kg", "Capsicum", "Carrot - 1 Kg", "Cashews - 1 Kg"]
 
 driver.find_element_by_css_selector("input.search-keyword").send_keys("ca")
 time.sleep(4)
 count_assignment = len(driver.find_elements_by_xpath("//div[@class='products']/div"))
 assert count_assignment == 4
 assignment_list = []
+final_list = []
 products = driver.find_elements_by_xpath("//div[@class='products']/div")
 for product in products:
     assignment_list.append(product.find_element_by_xpath("h4").text)
-print(assignment_list)
-assert assignment_list[0] == "Cauliflower - 1 Kg"
-assert assignment_list[1] == 'Carrot - 1 Kg'
-assert assignment_list[2] == 'Capsicum'
-assert assignment_list[3] == 'Cashews - 1 Kg'
-
-
-driver.find_element_by_css_selector("input.search-keyword").clear()
-driver.find_element_by_css_selector("input.search-keyword").send_keys("ber")
-time.sleep(4)
-count = len(driver.find_elements_by_xpath("//div[@class='products']/div"))
-assert count == 3
-buttons = driver.find_elements_by_xpath("//div[@class='product-action']/button")
-# //div[@class='product-action']/button/parent::div/parent::div/h4
-# we combine the previous xpath with one in the comment and in the loop
-for button in buttons:
-    items_list.append(button.find_element_by_xpath("parent::div/parent::div/h4").text)
-    button.click()
-print(items_list)
-
-driver.find_element_by_css_selector("img[alt='Cart']").click()
-driver.find_element_by_xpath("//button[text()='PROCEED TO CHECKOUT']").click()
-wait = WebDriverWait(driver, 6)
-wait.until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "promoCode")))
-
-veggies = driver.find_elements_by_css_selector("p.product-name")
-for veg in veggies:
-    veg_list.append(veg.text)
-
-print(veg_list)
-
-assert veg_list == items_list # Test Case number 1
-
-originalAmount = driver.find_element_by_css_selector(".discountAmt").text
-driver.find_element_by_class_name("promoCode").send_keys("rahulshettyacademy")
-driver.find_element_by_css_selector(".promoBtn").click()
-
-wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, "span.promoInfo")))
-discountedAmount = driver.find_element_by_css_selector(".discountAmt").text
-assert float(discountedAmount) < float(originalAmount) # Test Case Number 2
-
-print(driver.find_element_by_css_selector("span.promoInfo").text)
-
-amounts = driver.find_elements_by_xpath("//tr/td[5]/p")
-total = 0
-for amount in amounts:
-    total = total + int(amount.text)
-
-totalAmount = int(driver.find_element_by_css_selector(".totAmt").text)
-assert total == totalAmount # Test Case Number 3
-driver.close()
+sort_assignment_list = sorted(assignment_list)
+print(sort_assignment_list)
+step = 0
+while step < count_assignment:
+    for item in validation_list:
+        if validation_list[step] in item:
+            final_list.append(validation_list[step])
+            break
+    step += 1
+sort_final_list = sorted(final_list)
+print(sort_final_list)
+assert sort_assignment_list == sort_final_list
+driver.quit()
