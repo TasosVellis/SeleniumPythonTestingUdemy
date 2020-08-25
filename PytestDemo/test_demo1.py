cffb4883d257c467c6c6d4386536d8c301c59c5e
TasosVellis <anastasios.vellis@gmail.com>
2020-08-26 02:27:49 +0300

    pytest start

---
+++ PytestDemo/test_demo1.py
@@ -0,0 +1,6 @@
+# Any pytest file should start with test_ or end with _test
+# pytest method names should start with test
+# Any method should be wrapped in method
+
+def test_first_program():
+    print("Hello")
